16fb6794bf3e9a1d952f00cf255bb81bf87eff3a
Arsema <ama10415@nyu.edu>
2025-06-10 22:20:40 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,104 @@
+const express=require('express')
+const morgan=require('morgan')
+const app=express()
+const cors = require('cors')
+
+app.use(cors())
+let persons=[
+    { 
+      id: '1',
+      name: 'Arto Hellas', 
+      number: '040-123456',
+    },
+    { 
+      id: '2',
+      name: 'Ada Lovelace', 
+      number: '39-44-5323523',
+    },
+    { 
+      id: '3',
+      name: 'Dan Abramov', 
+      number: '12-43-234345',
+    },
+    { 
+      id: '4',
+      name: 'Mary Poppendieck', 
+      number: '39-23-6423122',
+    }
+]
+app.use(morgan('dev'))
+app.use(express.json())
+
+app.get('/api/persons', (request, response)=>{
+    response.json(persons)
+})
+app.get('/info',(request,response)=>{
+    const sizePersons=persons.length
+    const timeStamp= new Date()
+    response.json({sizePersons, timeStamp})
+})
+app.get('/api/persons/:id', (request,response)=>{
+    const id= request.params.id
+    const person= persons.find(person=>person.id===id)
+    if (person){
+        response.json(person)
+    }else{
+        response.json(404).end
+    }
+})
+app.delete('/api/persons/:id',(request,response)=>{
+    const id= request.params.id
+    persons=persons.filter(person=>person.id!==id)
+    response.status(204).end()
+})
+// const generateId=()=>{
+//     const maxId=persons.length>0
+//     ?Math.max(...persons.map(p=>Number(p.id))):0
+//     return String(maxId+1)
+// }
+const generateId = () => {
+  const maxId = persons.length > 0
+    ? Math.max(...persons.map(p => Number(p.id)))
+    : 0
+  return String(maxId + 1)
+}
+app.post('/api/persons', (request, response) => {
+  const body = request.body
+
+  if (!body.name) {
+    return response.status(400).json({ 
+      error: 'name missing' 
+    })
+  }
+
+  const person = {
+    name: body.name,
+    number: body.number,
+    id: generateId(),
+  }
+
+  persons = persons.concat(person)
+
+  response.json(person)
+})
+// app.post('/api/persons',(request,response)=>{
+//     const body= request.body
+//     // if(!body.name || !body.number){
+//     //     return response.status(400).json({
+//     //         error:'name or number missing'
+//     //     })
+//     // }
+//     const person={
+//         id: generateId(),
+//         name: body.name, 
+//         number: body.number,
+//     }
+//     persons=persons.concat(person)
+//     response.json(person)
+
+// })
+
+const PORT=3004
+app.listen(PORT, ()=>{
+    console.log(`server running on port ${PORT}`)
+})
